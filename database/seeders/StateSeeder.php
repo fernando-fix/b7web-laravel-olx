ea6314f9c2b2d4a1987dbd072007cc6ee6aebd90
Fernando de Lima Rodrigues <fernando_tecti@hotmail.com>
2025-01-24 23:13:06 -0300

    State seeder

---
+++ database/seeders/StateSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\State;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $states = [
+            "AC" => "Acre",
+            "AL" => "Alagoas",
+            "AM" => "Amazonas",
+            "AP" => "Amapá",
+            "BA" => "Bahia",
+            "CE" => "Ceará",
+            "DF" => "Distrito Federal",
+            "ES" => "Espírito Santo",
+            "GO" => "Goiás",
+            "MA" => "Maranhão",
+            "MG" => "Minas Gerais",
+            "MS" => "Mato Grosso do Sul",
+            "MT" => "Mato Grosso",
+            "PA" => "Pará",
+            "PB" => "Paraíba",
+            "PE" => "Pernambuco",
+            "PI" => "Piauí",
+            "PR" => "Paraná",
+            "RJ" => "Rio de Janeiro",
+            "RN" => "Rio Grande do Norte",
+            "RO" => "Rondônia",
+            "RR" => "Roraima",
+            "RS" => "Rio Grande do Sul",
+            "SC" => "Santa Catarina",
+            "SE" => "Sergipe",
+            "SP" => "São Paulo",
+            "TO" => "Tocantins",
+        ];
+
+        foreach ($states as $key => $value) {
+            $state = State::where('uf', $key)->first();
+
+            if (!$state) {
+                State::create([
+                    'uf' => $key,
+                    'name' => $value,
+                ]);
+            }
+        }
+    }
+}
