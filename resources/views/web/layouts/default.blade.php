814a332178ee258aa0827a1d5e477b34e2ad0fd0
Fernando de Lima Rodrigues <fernando_tecti@hotmail.com>
2025-03-19 22:17:44 -0300

    Vite install

--- resources/views/web/layouts/default.blade.php
+++ resources/views/web/layouts/default.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite(['resources/css/app.scss'])
     <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&family=Open+Sans:ital@0;1&family=Oswald:wght@400;700&display=swap"
         rel="stylesheet" />
